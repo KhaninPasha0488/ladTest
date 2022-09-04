794f2561266e87df3796ce46db40359290abd855
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-09-04 18:05:33 +0300

    create app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ActivityIndicator, Button, Pressable, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App</Text>
+      <ActivityIndicator size="large" />
+      <Button
+          onPress={()=>{}}
+          title="Вход"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+      />
+      <Pressable onPress={()=>{}}>
+        <Text>I'm pressable!</Text>
+      </Pressable>
+
+      <TouchableOpacity>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eeb4d8',
     alignItems: 'center',
     justifyContent: 'center',
   },
